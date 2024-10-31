ca1586b5dcd125d6448c1acb389bd6a98b1fb669
Kristian Bellò <kristian.bello@gatedev.com>
2024-10-31 11:01:59 +0100

    new branches

--- branches.ts
+++ branches.ts
@@ -1,8 +1,8 @@
 /*
 Aggiornare eventualmente il branch MVP
  */
-export const mvpBranch = 'feature/249646_219968_219966';
-export const stream2ABranch = 'feature/249649_236339_236340';
+export const mvpBranch = 'feature/250680_219968_219966';
+export const stream2ABranch = 'feature/250681_236339_236340';
 export const stream3ABranch = 'feature/236348_236342_236341';
 
 export const deloitteModules = [
