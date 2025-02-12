6853d364724b45705bc056dc732fd0b51c16224c
Kristian Bellò <kristian.bello@gatedev.com>
2025-02-12 18:31:51 +0100

    Branch 258422_236340_236339

--- branches.ts
+++ branches.ts
@@ -1,10 +1,10 @@
 /*
 Aggiornare eventualmente il branch MVP
  */
-export const stream23Branch = 'feature/257817_236340_236339';
+export const stream23Branch = 'feature/258422_236340_236339';
 export const areaPersonaleBranch = 'feature/242909_230387_242908';
 
-export const areaDocumentiBranch = 'feature/257817_236340_236339';
+export const areaDocumentiBranch = 'feature/258422_236340_236339';
 
 export const deloitteModules = [
     'ib_flutter_lib_a11y_utils',
