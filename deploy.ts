73a723ae4c31e201518e7b49076eda33189bdb30
Claudio.Zaffiro <claudio.zaffiro@consulenti.mediolanum.it>
2025-02-20 10:06:07 +0100

    New branch 258422_236340_236339

--- deploy.ts
+++ deploy.ts
@@ -339,8 +339,8 @@ async function createBranches() {
         //let newBranch = 'feature/251418_219968_219966';
         //let sourceBranch = 'feature/250680_219968_219966';
 
-        let newBranch = 'feature/256282_236339_236340';
-        let sourceBranch = 'feature/255592_236339_236340';
+        let newBranch = 'feature/258992_236340_236339';
+        let sourceBranch = 'feature/258422_236340_236339';
 
 
         let url = `https://git.gbm.lan/api/v4/projects/${module.gitlabProjectId}/repository/branches?private_token=${gitlabToken}&branch=${newBranch}&ref=${sourceBranch}`;
