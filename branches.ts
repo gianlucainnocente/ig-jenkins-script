bb1740febbfb7fbe6b761a1b78fde0ba9bd2e95e
Claudio.Zaffiro <claudio.zaffiro@consulenti.mediolanum.it>
2025-03-26 19:51:46 +0100

    New branch 261531_261621_261622

--- branches.ts
+++ branches.ts
@@ -1,10 +1,10 @@
 /*
 Aggiornare eventualmente il branch MVP
  */
-export const stream23Branch = 'feature/260994_259889_259887';
+export const stream23Branch = 'feature/261531_261621_261622';
 export const areaPersonaleBranch = 'feature/242909_230387_242908';
 
-export const areaDocumentiBranch = 'feature/260994_259889_259887';
+export const areaDocumentiBranch = 'feature/261531_261621_261622';
 
 export const deloitteModules = [
     'ib_flutter_lib_a11y_utils',
